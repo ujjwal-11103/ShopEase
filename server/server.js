b7ca91a40ab5653f27963cf4e31013aacfa4c466
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2024-06-18 12:37:30 +0530

    Created an expess application

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import colors from "colors"
+import dotenv from "dotenv"
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./Routes/authRoute.js";
+// config dotenv
+dotenv.config();
+
+// DB config
+connectDB();
+
+//rest object
+const app = express();
+
+// middleware
+app.use(express.json())
+app.use(morgan('dev'))
+
+// routes
+app.use('/api/v1/auth', authRoute)
+
+// rest APi
+app.get('/', (req, res) => {
+    res.send(`Connected to server succesffuly on ${process.env.Dev_mode} mode`);
+})
+
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${process.env.Dev_mode} on port = ${PORT}`.bgCyan.white);
+})
